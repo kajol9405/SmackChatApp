739aea076b26ea7a1cbf036d113956b301a4ea66
Kajol <kajol9405@gmail.com>
2018-02-19 20:01:31 -0600

    mvc and api dec

--- app/src/main/java/com/example/kajol/smackchatapp/Controller/LoginActivity.kt
+++ app/src/main/java/com/example/kajol/smackchatapp/Controller/LoginActivity.kt
@@ -1,9 +1,10 @@
-package com.example.kajol.smackchatapp
+package com.example.kajol.smackchatapp.Controller
 
 import android.content.Intent
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
 import android.view.View
+import com.example.kajol.smackchatapp.R
 
 class LoginActivity : AppCompatActivity() {
 
@@ -17,7 +18,7 @@ class LoginActivity : AppCompatActivity() {
     }
     fun loginCreateUserBtnClicked(view : View){
 
-        val createUserIntent = Intent(this,createUserActivity::class.java)
+        val createUserIntent = Intent(this, createUserActivity::class.java)
         startActivity(createUserIntent)
 
     }
